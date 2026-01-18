c2f56bebe7df89cd94f47b7d9a8add7a2f57d9cc
Roberto L. Jr. <gsn.roberto@gmail.com>
2026-01-18 19:22:03 -0300

    feat: implement BCryptPassword, replace raw password with encrypted password in seeding file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$Pnh58SUZ9U3CJHC3IPE9R.uS/ZjiqN21GMc3NAYA4XUjwiGv0MUWi');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$Pnh58SUZ9U3CJHC3IPE9R.uS/ZjiqN21GMc3NAYA4XUjwiGv0MUWi');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
